be051a8160a2237b2fb762de1d9f602cfa03e214
Wiktor Niesiobedzki <github@vink.pl>
2017-12-04 23:19:37 +0100

    Add tests

--- tests/generate_tests.py
+++ tests/generate_tests.py
@@ -1,21 +1,18 @@
 import os
+import pathlib
 
 from tests.tests_runner import get_merger
 
 
 def generate_tests():
-    directory = os.path.dirname(os.path.realpath(__file__))
-    for test in os.listdir(directory):
-        if os.path.samefile(test, __file__) or os.path.isfile(test) or test == '__pycache__' or (
-                os.path.isfile(os.path.join(directory, test, "result_full.xml")) and
-                os.path.isfile(os.path.join(directory, test, "result_incremental.xml"))
-        ):
-            continue
-        print("Generating tests for: {}".format(test))
-        with open(os.path.join(directory, test, "result_full.xml"), "wb+") as f:
-            f.write(get_merger(os.path.join(directory, test)).get_full_result())
-        with open(os.path.join(directory, test, "result_incremental.xml"), "wb+") as f:
-            f.write(get_merger(os.path.join(directory, test)).get_incremental_result())
+    directory = pathlib.Path(__file__).parent.parent / "testdata"
+    for test in directory.iterdir():
+        if test.is_dir() and not ((test / "result_full.xml").is_file() and (test / "result_incremental.xml").is_file()):
+            print("Generating tests for: {}".format(test))
+            with open(os.path.join(directory, test, "result_full.xml"), "wb+") as f:
+                f.write(get_merger(test).get_full_result())
+            with open(os.path.join(directory, test, "result_incremental.xml"), "wb+") as f:
+                f.write(get_merger(test).get_incremental_result())
 
 
 def main():
