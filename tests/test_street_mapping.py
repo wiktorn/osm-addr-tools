f0f30bbd0a9a530af600b9b1593083e00afd8188
Wiktor Niesiobedzki <github@vink.pl>
2018-04-01 11:35:44 +0200

    mapstreet unit tests

---
+++ tests/test_street_mapping.py
@@ -0,0 +1,17 @@
+import unittest
+
+from utils.mapping import mapstreet
+
+
+class TestStreetMapping(unittest.TestCase):
+    def test_plac_sloneczny(self):
+        self.assertEqual("Plac Słoneczny", mapstreet("Słoneczny", "40187"))
+
+    def test_poniatowskiego(self):
+        self.assertEqual("Księcia Józefa Poniatowskiego", mapstreet("Poniatowskiego", "17113"))
+
+    def test_waszyngtona(self):
+        self.assertEqual("Aleja Jerzego Waszyngtona", mapstreet("Aleja J. Waszyngtona", "45576"))
+
+    def test_waszyngtona_no_simc(self):
+        self.assertEqual("Aleja J. Waszyngtona", mapstreet("Aleja J. Waszyngtona", "xxx"))
